2c9a93e41cad2dd126ea06bc9a341e5bc0c01fee
Vishu9354 <vishu0883386@gmail.com>
2026-02-23 22:52:26 +0530

    update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,8 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
-              code.
+Hi, I’m Vishal — a frontend developer and engineer building scalable SaaS platforms with clean architecture and real-world impact.
             </p>
 
             <Button
